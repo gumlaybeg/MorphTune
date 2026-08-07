197113c236dc7c61d8cac6c27d0d70b6dd208ebd
gumlaybeg <gumlaybeg15@protonmail.com>
2026-08-08 02:22:37 +0800

    Refactor build.gradle.kts for Kotlin JVM
    Removed Android-specific configurations and switched to Kotlin JVM plugin.

--- innertube/build.gradle.kts
+++ innertube/build.gradle.kts
@@ -1,21 +1,6 @@
 plugins {
-    id("com.android.library")
+    kotlin("jvm")
     alias(libs.plugins.kotlin.serialization)
-}
-
-android {
-    namespace = "com.arturo254.innertube"
-    compileSdk = 37
-
-    defaultConfig {
-        minSdk = 26
-    }
-
-    compileOptions {
-        isCoreLibraryDesugaringEnabled = true
-        sourceCompatibility = JavaVersion.VERSION_21
-        targetCompatibility = JavaVersion.VERSION_21
-    }
 }
 
 kotlin {
@@ -32,10 +17,9 @@ dependencies {
     
     // Uses the alias from your existing libs.versions.toml
     implementation(libs.newpipe.extractor) 
-    // Rhino for JS Deobfuscation (since it wasn't in your TOML)
+    
+    // Rhino for JS Deobfuscation
     implementation("org.mozilla:rhino:1.7.15") 
     
     testImplementation(libs.junit)
-
-    coreLibraryDesugaring(libs.desugaring)
 }
