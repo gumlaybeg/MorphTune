d1f16e4e73ea165326a4a9b5fe90f97c670fb4bd
gumlaybeg <gumlaybeg15@protonmail.com>
2026-08-08 03:45:39 +0800

    Make video ID fields nullable and add thumbnail actions

--- innertube/src/main/java/com/arturo254/innertube/models/body/EditPlaylistBody.kt
+++ innertube/src/main/java/com/arturo254/innertube/models/body/EditPlaylistBody.kt
@@ -27,20 +27,34 @@ sealed class Action {
     @Serializable
     data class MoveVideoAction(
         val action: String = "ACTION_MOVE_VIDEO_BEFORE",
-        val setVideoId: String,
-        val movedSetVideoIdSuccessor: String
+        val setVideoId: String?,
+        val movedSetVideoIdSuccessor: String? = null
     ) : Action()
 
     @Serializable
     data class RemoveVideoAction(
         val action: String = "ACTION_REMOVE_VIDEO",
-        val setVideoId: String,
-        val removedVideoId: String
+        val setVideoId: String?,
+        val removedVideoId: String?
     ) : Action()
 
     @Serializable
     data class RenamePlaylistAction(
         val action: String = "ACTION_SET_PLAYLIST_NAME",
         val playlistName: String
+    ) : Action()
+
+    @Serializable
+    data class SetCustomThumbnailAction(
+        val action: String = "ACTION_SET_PLAYLIST_CUSTOM_THUMBNAIL",
+        val addedCustomThumbnail: AddedCustomThumbnail
+    ) : Action() {
+        @Serializable
+        data class AddedCustomThumbnail(val playlistScottyEncryptedBlobId: String)
+    }
+
+    @Serializable
+    data class RemoveCustomThumbnailAction(
+        val action: String = "ACTION_REMOVE_PLAYLIST_CUSTOM_THUMBNAIL"
     ) : Action()
 }
